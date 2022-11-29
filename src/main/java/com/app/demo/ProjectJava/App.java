0c0db9727337daaec36aca7c7b97ea03599eb6b9
pragna <pragnam58@gmail.com>
2022-11-29 11:39:01 +0530

    maven

---
+++ src/main/java/com/app/demo/ProjectJava/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.ProjectJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
